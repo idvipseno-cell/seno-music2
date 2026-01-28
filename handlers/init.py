edab544013ea20b327df9950e550c18817a98258
idvipseno-cell <idvipseno@gmail.com>
2026-01-28 13:33:38 +0300

    Create init.py

---
+++ handlers/init.py
@@ -0,0 +1,4 @@
+# Developed by Sino (@idseno)
+from .start import handle_start as start_handler
+from .music import handle_download, handle_play, handle_stop, handle_pause
+from .dev import handle_dev_panel, handle_broadcast, handle_force_sub
