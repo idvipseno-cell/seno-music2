69495076fc59725de9faca668b72dbcc4ca963c4
idvipseno-cell <idvipseno@gmail.com>
2026-01-28 13:10:22 +0300

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,7 @@
+# Developed by Sino (@idseno)
+from pyrogram import Client
+
+async def auto_leave_assistant(assistant):
+    # Logic to leave channels/groups after 300 seconds
+    # Implement based on active calls
+    pass  # Placeholder
